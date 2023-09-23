ee8f077ff55f5fa92c09e74972c82d86bc79222b
DmitriiBogolaev <dbogolaev@mail.ru>
2023-09-23 09:37:39 +0300

    message

---
+++ adadadda.py
@@ -0,0 +1,15 @@
+a = int(input())
+f = []
+f.append(str(a))
+while a!=0:
+    a = int(input())
+    f.append(str(a))
+f = f.remove('0')
+d=[]
+for i in range(len(f)-2):
+    if int(f[i]) > int(f[i - 1]) and int(f[i]) > int(f[i + 1]) and i != (len(f) - 1) and i != 0:
+        d.append(i)
+if len(d)<2:
+    print('0')
+else:
+    print(int(d[-1]) - int(d[0]) - 1)
